151f26a6819f71f5df4a84de59c8c69d326fb8bd
Gregory Izuka <gizuka@seamfix.com>
2024-04-06 23:53:55 +0100

    started message pane

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  @ViewChild('new_chat')
+  modal: ModalController = new ModalController;
+  @ViewChild('popover')
+  popover: PopoverController = new PopoverController;
+  segment = 'chats';
+  open_new_chat = false;
+  users = [
+    {id: 1, name: 'NIkhil', photo: 'https://i.pravatar.cc/315'},
+    {id: 2, name: 'XYZ', photo: 'https://i.pravatar.cc/325'},
+  ];
+  chatRooms = [
+    {id: 1, name: 'NIkhil', photo: 'https://i.pravatar.cc/315'},
+    {id: 2, name: 'XYZ', photo: 'https://i.pravatar.cc/325'},
+  ];
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.popover.dismiss();
+  }
+
+  onSegmentChanged(event: any) {
+    console.log(event);
+  }
+
+  newChat() {
+    this.open_new_chat = true;
+  }
+
+  onWillDismiss(event: any) {}
+
+  cancel() {
+    this.modal.dismiss();
+    this.open_new_chat = false;
+  }
+
+  startChat(item:any) {
+
+  }
+
+  getChat(item:any) {
+    this.router.navigate(['/', 'home', 'chats', item?.id]);
+  }
+
+}
